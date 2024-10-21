277c84e7484004893340906694ed7b6cc9b564d5
zezhen_fan <842618916@qq.com>
2024-10-21 14:21:17 +0800

    [WN] demo-maven-package

---
+++ demo-maven-package/README.md
@@ -0,0 +1,6 @@
+# demo-maven-package
+
+动态指定启动类
+```shell
+mvn package -Dstart-class=com.example.AnotherMainClass
+```
---
+++ demo-maven-package/src/main/java/com/github/fanzezhen/demo/mavenpackage/Main.java
@@ -0,0 +1,10 @@
+package com.github.fanzezhen.demo.mavenpackage;
+
+/**
+ * 主类
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("这是一个可执行的程序");
+    }
+}
